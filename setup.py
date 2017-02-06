84eb40793631a83be11c9ccdb2030999e7bf81dd
Markus Dod <markus.dod@ivm-sachsen.de>
2017-02-06 10:14:52 +0100

    some format changings

--- setup.py
+++ setup.py
@@ -1,40 +1,36 @@
 '''PyHook3: Python wrapper for out-of-context input hooks in Windows
 
-The PyHook3 package provides callbacks for global mouse and keyboard events in Windows. Python
-applications register event handlers for user input events such as left mouse down, left mouse up,
-key down, etc. and set the keyboard and/or mouse hook. The underlying C library reports information
-like the time of the event, the name of the window in which the event occurred, the value of the
-event, any keyboard modifiers, etc.
+The PyHook3 package provides callbacks for global mouse and keyboard 
+events in Windows. Python applications register event handlers for 
+user input events such as left mouse down, left mouse up, key down, 
+etc. and set the keyboard and/or mouse hook. The underlying C library 
+reports information like the time of the event, the name of the window 
+in which the event occurred, the value of the event, any keyboard 
+modifiers, etc.
 '''
 
-from setuptools import setup, find_packages, Extension
-from codecs import open
-from os import path
+from setuptools import setup, Extension
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-  long_description = f.read()
+doclines = __doc__.split('\n')
 
 setup(
     name='PyHook3',
-    version='1.6.0',
+    version='1.6.1',
 
     description='Python wrapper for out-of-context input hooks in Windows',
-    long_description=long_description,
+    long_description=' '.join(doclines[2:]),
     url='https://github.com/gggfreak2003/PyHook3',
     author='Peter Parente, Markus Dod',
     author_email='mdod@hs-mittweida.de',
     license='MIT',
 
     classifiers=[
-        'Development Status :: 5 - Stable',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: System :: Monitoring',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Operating System :: Microsoft :: Windows'
+        'Operating System :: Microsoft :: Windows',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
